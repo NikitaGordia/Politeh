a052bd17e27323fa2b927edd12747b433512eaef
NikitaGordia <nikita.gordia@gmail.com>
2018-05-08 09:31:24 +0300

    Init

---
+++ app/src/main/java/com/nikitagordia/politeh/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.nikitagordia.politeh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
